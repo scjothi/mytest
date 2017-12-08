daeb3c63c600d3bb852318f6a0384b1db4b5caf9
scjothi <scjothi@gmail.com>
2017-12-08 02:59:17 -0500

    test

---
+++ MyTestProject/src/com/scj/ws/client/MyTestWSClient.java
@@ -0,0 +1,8 @@
+package com.scj.ws.client;
+
+
+//https://api14uat.clarityssi.net:446/AmerigroupMedicaidWebService/wsdl/e_service.wsdl
+//https://api14uat.clarityssi.net/AmerigroupMedicaidWebService/wsdl/e_service.wsdl
+public class MyTestWSClient {
+
+}
